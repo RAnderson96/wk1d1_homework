fac900f5d460d4dd74c60137030595d05aeef8fa
RAnderson96 <admin@Admins-MacBook-Pro.local>
2022-10-17 18:48:19 +0100

    Fixed error where thougt everything was a cabbage & adjust func name

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,16 +1,16 @@
-def precourse_work(cabbage):
+def is_cabbage(cabbage):
     cabbages = ["Green Cabbage", "Red Cabbage", "Purple Cabbage", "Savoy Cabbage", "Napa Cabbage"]
     for val in cabbages:
-        if val == cabbage or val == "Cabbage" or "cabbage": 
-            cab = "Thats a cabbage!!"
+        if val == cabbage or val == "Cabbage" or val == "cabbage": 
+            cab = "Thats a cabbage!!!!"
         else:
-            cab = "Not so sure about that one"
+            cab = "I don't think thats a cabbage..."
     return cab
 
     
             
 
-test = precourse_work("cabbage")
+test = is_cabbage("Rory")
 
 
 print(test)
