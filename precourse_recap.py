f4f48ad77e2d356dc6e162f3e00c9e246f37b2e5
RAnderson96 <admin@Admins-MacBook-Pro.local>
2022-10-30 16:42:20 +0000

    refactored code

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,18 +1,15 @@
-def is_cabbage(cabbage):
-    testlist = ["green cabbage", "red cabbage", "purple cabbage", "savoy cabbage", "napa cabbage"]
-    for val in testlist:
-        if val == cabbage.lower() or val == "cabbage": 
+def cabbage_check(cabbage):
+    cabbage_list = ["green cabbage", "red cabbage", "purple cabbage", "savoy cabbage", "napa cabbage", "cabbage"]
+    for val in cabbage_list:
+        if val == cabbage.lower():
+            cab = f"{cabbage} is a cabbage!!!!"
+            return cab
+    return f"I don't think that {cabbage} is a cabbage..."
 
-            cab = "Thats a cabbage!!!!"
-            break
-        else:
-            cab = "I don't think thats a cabbage..."
-    return cab
+test = cabbage_check("Ed")
 
-    
-            
-
-test = is_cabbage("sAvoY cAbbage")
-
+test2 = cabbage_check("Red Cabbage")
 
 print(test)
+print(test2)
+
